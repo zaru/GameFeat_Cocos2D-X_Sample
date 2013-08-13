e1524576f04c77b80222b615d7109aa9068f8db3
sakuraba <sakuraba@basicinc.jp>
2013-08-13 12:47:35 +0900

    アイコン型広告の実装サンプルコードを追加

--- Cocos2dxSample/proj.android/src/jp/basicinc/sample/cocos2dx/Cocos2dxSample.java
+++ Cocos2dxSample/proj.android/src/jp/basicinc/sample/cocos2dx/Cocos2dxSample.java
@@ -23,14 +23,51 @@ THE SOFTWARE.
 ****************************************************************************/
 package jp.basicinc.sample.cocos2dx;
 
+import jp.basicinc.gamefeat.android.sdk.controller.GameFeatIconAdLoader;
+import jp.basicinc.gamefeat.android.sdk.view.GameFeatIconView;
+
 import org.cocos2dx.lib.Cocos2dxActivity;
 
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.widget.RelativeLayout;
+import android.widget.RelativeLayout.LayoutParams;
 
 public class Cocos2dxSample extends Cocos2dxActivity{
+	
+	private static Cocos2dxSample me = null;
+	
+    static final int bannerViewId = 0x661ad306;
+    static GameFeatIconAdLoader myIconAdLoader;
 
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
+		
+		me = this;
+		myIconAdLoader = new GameFeatIconAdLoader();
+		
+		DisplayMetrics metrics = new DisplayMetrics();  
+		me.getWindowManager().getDefaultDisplay().getMetrics(metrics);
+		
+		// アイコン広告のサイズ（解像度に合わせて調整）
+		int width = (int) (114 * metrics.density);
+		int height = (int) (114 * metrics.density);
+
+    	GameFeatIconView iconView = (GameFeatIconView)me.findViewById(bannerViewId);
+    	if (iconView == null) {
+    		RelativeLayout layout = new RelativeLayout(me);
+    		me.addContentView(layout, new LayoutParams(width, height));
+            
+            iconView = new GameFeatIconView(me);
+            iconView.addLoader(myIconAdLoader);
+            iconView.setId(bannerViewId);
+            
+            layout.addView(iconView);
+            
+    	}
+    	
+    	// アイコン広告の表示
+        myIconAdLoader.loadAd(me);
 	}
 	
     static {
